70f31ce9e73f06fbff2da828cd38c4a201248572
Agnieszka Moscichowska <agnieszka.moscichowska@miquido.com>
2017-08-05 22:42:01 +0200

    added Charts

---
+++ Carthage/Build/iOS/Charts.framework/Headers/Charts-Swift.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1127570a9a9ef8ff684c7e416bc654ca79edfd944df98bb852ef6fc6e8826688
+size 212625
---
+++ Carthage/Build/iOS/Charts.framework/Headers/Charts.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ae0b3b9f8095b32e6f0292e4734ec44db7bf7cb2ad348144dff6bf769c758936
+size 744
